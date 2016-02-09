93c4cd5b6383d700eff007d61f22a094ae79a849
Edwardthedog2 <Edwardthedog2@users.noreply.github.com>
2016-02-09 17:54:38 -0600

    Create MainClass.php
    Code provided, and explained by HittmanA. Big thanks to him!

---
+++ src/MainClass.php
@@ -0,0 +1,34 @@
+<?php
+namespace SAM\SAM;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+
+class MainClass extends PluginBase implements Listener{
+    public function onEnable()
+    {
+        $this->getLogger()->info("[SAM] SAM v0.0.1 enabled");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onDisable()
+    {
+        $this->getLogger()->info("[SAM] SAM v0.0.1 disabled");
+    }
+
+    function onJoin(PlayerJoinEvent $event)
+    {
+        $name = $event->getPlayer()->getDisplayName();    
+        if ($registered = FALSE){
+        $name->sendMessage("[SAM] Hello " . $name . " this server use SAM by Edwardthedog2. You must register at " .          $url . " to play");  
+    }
+    if ($registered = TRUE){
+        //$registered is whether or not the player is registered. Set it to retrieve the players info from the $url
+        $name->sendMessage("[SAM] Hello" . $name . " To play you must login with /login (your password)");  
+       //$url is the url of the users website
+    }
+  }
+}
