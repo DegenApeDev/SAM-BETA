5fa7acf41a81b17d6a5cea895ee999b0828b7753
Edwardthedog2 <Edwardthedog2@users.noreply.github.com>
2016-02-14 15:30:12 -0600

    Update web.php

--- src/SAM/web.php
+++ src/SAM/web.php
@@ -61,28 +61,4 @@ $login = fopen("data.json", "w+");
 fwrite($login, json_encode(array("username" => $username, "pswd" => $password)));
 fclose($login);
 
-class MainClass extends PluginBase implements Listener{
-    public function onEnable()
-    {
-        $this->getLogger()->info("[SAM] SAM v0.0.1 enabled");
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
-    public function onDisable()
-    {
-        $this->getLogger()->info("[SAM] SAM v0.0.1 disabled");
-    }
-    function onJoin(PlayerJoinEvent $event)
-    {
-        $name = $event->getPlayer()->getDisplayName();    
-        if ($registered = FALSE){
-        $name->sendMessage("[{$this->getConfig()->get("prefix")}] Hello " . $name . " this server use SAM by EndKingdom. You must register at " .          $url . " to play");  
-    }
-    if ($registered = TRUE){
-        //$registered is whether or not the player is registered. Set it to retrieve the players info from the $url
-        $name->sendMessage("[{$this->getConfig()->get("prefix")}] Hello" . $name . " To play you must login with /login (your password)");  
-       //$url is the url of the users website
-    }
-  }
-}
-
 ?>
